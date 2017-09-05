25a2865bbc73e4f78bfcbd1b1059e6d7b680d4d4
Warren Moore <warren@wamonite.com>
2017-09-05 17:06:10 +0100

    Support multiple build targets and per-target run argument.

--- docker_utils.sh
+++ docker_utils.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -eu
 
-# 2017-06-21 17:17
+# 2017-09-05 12:22
 
 script_path=$(cd $(dirname $0); pwd -P)
 
 . "${script_path}/config.sh"
 
-# minimum requirements
-[ -z "${container_name:-}" ] && { echo "container_name not set" 1>&2 ; exit 1 ; }
-[ ! -d "${script_path}/src" ] && { echo "src directory not found" 1>&2 ; exit 1 ; }
+# check for src dir
+[[ ! -d "${script_path}/src" ]] && { echo "src directory not found" 1>&2 ; exit 1 ; }
 
-# defaults
-repo_name=${DOCKER_REPO:-}
-image_name="${repo_name:+${repo_name}/}${container_name}"
+# docker_cmd and container_hostname
 docker_cmd=${DOCKER_CMD:-docker}
 if [[ ! -z "${DOCKER_HOST:-}" && "${DOCKER_HOST}" =~ ^tcp://(.*):[0-9]*$ ]]
 then
@@ -21,13 +18,39 @@ fi
 
 # for a symlink name <prefix>_<type>.sh echo <type> or nothing
 function get_link_type() {
-    script_prefix="${1:-}"
-    [ -z "$script_prefix" ] && { echo "get_link_type script_prefix not set" 1>&2 ; exit 1 ; }
+    local script_prefix="${1:-}"
+    local script_test="${2:-}"
+    [[ -z "$script_prefix" ]] && { echo "get_link_type script_prefix not set" 1>&2 ; exit 1 ; }
+    [[ -z "$script_test" ]] && { echo "get_link_type script_test not set" 1>&2 ; exit 1 ; }
 
-    if [[ "$script_name" =~ ${script_prefix}_(.*)\.sh$ ]]
+    if [[ "$script_test" =~ ${script_prefix}_(.*)\.sh$ ]]
     then
         echo "${BASH_REMATCH[1]}"
     fi
+}
+
+# for a given link type, generate the container name
+function get_container_name() {
+    local link_type_val="${1:-}"
+    local container_name_val="${container_name:-}"
+    if [[ ! -z "${link_type_val}" ]]
+    then
+        local container_name_name="container_name_${link_type_val}"
+        [[ ! -z "${!container_name_name:-}" ]] && container_name_val="${!container_name_name}"
+    fi
+
+    # check for valid container name
+    [[ -z "${container_name_val}" ]] && { echo "container_name not set" 1>&2 ; exit 1 ; }
+
+    echo "${container_name_val}"
+}
+
+# for a given container name, generate the image name
+function get_image_name() {
+    local container_name_val="${1:-}"
+    local repo_name=${DOCKER_REPO:-}
+
+    echo "${repo_name:+${repo_name}/}${container_name_val}"
 }
 
 # vars may have changed, so echo the latest start arguments
@@ -39,39 +62,59 @@ function get_host_args() {
 script_name=$(basename $0)
 case "$script_name" in
 build_image*)
-    echo "#### BUILD ${image_name}"
-    exec ${docker_cmd} build --rm -t "${image_name}" "${script_path}/src"
+    link_type=$(get_link_type "build_image" "${script_name}")
+    container_name=$(get_container_name "${link_type}")
+    image_name=$(get_image_name "${container_name}")
+
+    if [[ ! -z "${link_type}" ]]
+    then
+        build_args_name="build_args_${link_type}"
+        [[ ! -z "${!build_args_name:-}" ]] && build_args="${!build_args_name}"
+    fi
+
+    echo "#### BUILD ${image_name}${build_args:+ '${build_args}'}"
+    exec ${docker_cmd} build ${build_args:-} --rm -t "${image_name}" "${script_path}/src"
     ;;
 
 start_container*)
-    link_type=$(get_link_type "start_container")
+    link_type=$(get_link_type "start_container" "${script_name}")
+    container_name=$(get_container_name "${link_type}")
+    image_name=$(get_image_name "${container_name}")
 
-    if [ ! -z "${link_type}" ]
+    if [[ ! -z "${link_type}" ]]
     then
         container_name="${container_name}_${link_type}"
 
         docker_args_name="docker_args_${link_type}"
-        docker_args="${!docker_args_name}"
+        [[ ! -z "${!docker_args_name:-}" ]] && docker_args="${!docker_args_name}"
+
+        command_args_name="command_args_${link_type}"
+        [[ ! -z "${!command_args_name:-}" ]] && command_args="${!command_args_name}"
+
+        daemon_args_name="daemon_args_${link_type}"
+        [[ ! -z "${!daemon_args_name:-}" ]] && daemon_args="${!daemon_args_name}"
     fi
 
-    if [ $# -ne 0 ]
+    if [[ $# -ne 0 ]]
     then
         echo "#### START ${container_name} '$@'"
         exec ${docker_cmd} run --rm -ti ${docker_args:-} $(get_host_args) ${image_name} $@
-    elif [ ! -z "${container_daemon:-}" ]
+    elif [[ ! -z "${container_daemon:-}" ]]
     then
         echo "#### START DAEMON ${container_name}"
         exec ${docker_cmd} run -d --restart unless-stopped ${docker_args:-} $(get_host_args) ${image_name} ${daemon_args:-}
     else
-        echo "#### START ${container_name}"
-        exec ${docker_cmd} run --rm ${docker_args:-} $(get_host_args) ${image_name}
+        echo "#### START ${container_name}${command_args:+ '${command_args}'}"
+        exec ${docker_cmd} run --rm ${docker_args:-} $(get_host_args) ${image_name} ${command_args:-}
     fi
     ;;
 
 stop_container*)
-    link_type=$(get_link_type "stop_container")
+    link_type=$(get_link_type "stop_container" "${script_name}")
+    container_name=$(get_container_name "${link_type}")
+    image_name=$(get_image_name "${container_name}")
 
-    if [ ! -z "${link_type}" ]
+    if [[ ! -z "${link_type}" ]]
     then
         container_name="${container_name}_${link_type}"
     fi
@@ -85,9 +128,11 @@ stop_container*)
     ;;
 
 tail_logs*)
-    link_type=$(get_link_type "tail_logs")
+    link_type=$(get_link_type "tail_logs" "${script_name}")
+    container_name=$(get_container_name "${link_type}")
+    image_name=$(get_image_name "${container_name}")
 
-    if [ ! -z "${link_type}" ]
+    if [[ ! -z "${link_type}" ]]
     then
         container_name="${container_name}_${link_type}"
     fi
@@ -97,23 +142,27 @@ tail_logs*)
     ;;
 
 push_image*)
+    link_type=$(get_link_type "push_image" "${script_name}")
+    container_name=$(get_container_name "${link_type}")
+    image_name=$(get_image_name "${container_name}")
+
     echo '#### PUSH'
 
     version="${1:-}"
 
-    if [ -z "${version}" ]
+    if [[ -z "${version}" ]]
     then
         exec ${docker_cmd} images | grep "^${image_name} "
 
     else
-        if [ "${version}" != "latest" ]
+        if [[ "${version}" != "latest" ]]
         then
             ${docker_cmd} tag "${image_name}:latest" "${image_name}:${version}"
         fi
 
         ${docker_cmd} push "${image_name}:${version}"
 
-        if [ "${version}" != "latest" ]
+        if [[ "${version}" != "latest" ]]
         then
             git tag -a "${version}" -m "${image_name}:${version}"
         fi
@@ -121,6 +170,10 @@ push_image*)
     ;;
 
 initialise_*)
+    # TODO does not currently support link types
+    container_name=$(get_container_name "")
+    image_name=$(get_image_name "${container_name}")
+
     [[ ! "${script_name}" =~ initialise_(.*)\.sh$ ]] && { echo 'unable to determine volume' 1>&2 ; exit 1 ; }
     volume_id="${BASH_REMATCH[1]}"
 
@@ -130,7 +183,7 @@ initialise_*)
     volume_dest="${!volume_dest_var}"
     volume_src="${1:-}"
 
-    [ -z "${volume_src}" ] && { echo "src directory not provided" 1>&2 ; exit 1 ; }
+    [[ -z "${volume_src}" ]] && { echo "src directory not provided" 1>&2 ; exit 1 ; }
 
     container_name="${container_name}_${volume_name}"
     echo "#### INITIALISE ${container_name} (${volume_src} -> ${volume_dest})"
@@ -142,6 +195,10 @@ initialise_*)
     ;;
 
 backup*)
+    # TODO does not currently support link types
+    container_name=$(get_container_name "")
+    image_name=$(get_image_name "${container_name}")
+
     [[ ! "${script_name}" =~ backup_(.*)\.sh$ ]] && { echo 'unable to determine volume' 1>&2 ; exit 1 ; }
     volume_id="${BASH_REMATCH[1]}"
 
@@ -157,8 +214,7 @@ backup*)
     ;;
 
 *)
-    echo "#### CONFIG ${container_name}"
-    echo "container_name: $container_name"
-    echo "repo_name: $repo_name"
+    echo "#### ERROR unsupported invocation: ${script_name}" 1>&2
+    exit 1
     ;;
 esac
